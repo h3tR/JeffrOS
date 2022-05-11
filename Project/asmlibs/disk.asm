4083375568c9bd9e6d195970038d630dda26b96d
h3tR3m1s <66027579+h3tR3m1s@users.noreply.github.com>
2022-05-11 19:59:53 +0200

    restore required files + 32bit progress
    - restored disk.asm, print.asm and boot.asm
    - changed print.asm to not use interrupts and to  32-bits
    -removed print_hexadecimal from print.asm

---
+++ Project/asmlibs/disk.asm
@@ -0,0 +1,22 @@
+; requires an include of the print.asm library
+disk_load:
+    push dx
+
+    mov ah, 0x02
+    mov al, dh
+    mov ch, 0x00
+    mov dh, 0x00
+    mov cl, 0x02
+
+    int 0x13
+
+    jc disk_error
+
+
+disk_error:
+    mov bx, DISK_ERROR_MSG
+    call print_string
+    jmp $
+
+DISK_ERROR_MSG:
+    db "Disk read error!",0
